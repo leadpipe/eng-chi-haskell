51fd9b1c9f6e9dbb56bce10ab4e1f761665cbab2
sussman <sussman@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-01-16 06:26:56 +0000

    Finish all but the last 3 exercises.

--- sussman/ch03/general.hs
+++ sussman/ch03/general.hs
@@ -50,7 +50,64 @@ mySort xs = sortBy lenCompare xs
 -- 7. a function that joins a list of lists together using a separator value
 
 myintersperse :: a -> [[a]] -> [a]
-myintersperse sep l
-   | l == []        = []
-   | length l == 1  = head l
-   | otherwise      = head l ++ sep ++ myintersperse sep (tail l)
+myintersperse sep l = 
+  case l of
+     []           -> []
+     [a]          -> a
+     (x:xs)       -> x ++ sep : myintersperse sep xs
+
+
+-- 8.  Using the binary tree type that we defined earlier in this
+-- chapter, write a function that will determine the height of the
+-- tree. The height is the largest number of hops from the root to an
+-- Empty. For example, the tree Empty has height zero; Node "x" Empty
+-- Empty has height one; Node "x" Empty (Node "y" Empty Empty) Has
+-- height two; and so on.
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
+
+treeHeight :: Tree a -> Integer
+treeHeight t =
+  case t of
+     Empty                  -> 0
+     Node a b c             -> 1 + treeHeight b + treeHeight c
+
+
+-- try evaluating: treeHeight (Node "x" Empty (Node "y" Empty (Node "z" (Node "q"
+-- Empty Empty) (Node "r" Empty Empty))))
+
+
+-- 9. Consider three two-dimensional points a, b, and c. If we look at
+-- the angle formed by the line segment from a to b and the line segment
+-- from b to c, it either turns left, turns right, or forms a straight
+-- line. Define a Direction data type that lets you represent these
+-- possibilities.
+
+data Point = Point Int Int
+             deriving (Show)
+
+data Direction = LeftTurn | RightTurn | StraightAhead
+                 deriving (Show)
+
+
+-- 10.  Write a function that calculates the turn made by three 2D
+-- points and returns a Direction.
+
+calculateTurn :: Point -> Point -> Point -> Direction
+calculateTurn a b c = LeftTurn
+-- finish this
+
+
+-- 11.  Define a function that takes a list of 2D points and computes the
+-- direction of each successive triple. Given a list of points
+-- [a,b,c,d,e], it should begin by computing the turn made by [a,b,c],
+-- then the turn made by [b,c,d], then [c,d,e]. Your function should
+-- return a list of Direction.
+
+
+-- 12.  Using the code from the preceding three exercises, implement
+-- Graham's scan algorithm for the convex hull of a set of 2D
+-- points. You can find good description of what a convex hull. is,
+-- and how the Graham scan algorithm should work, on Wikipedia.
