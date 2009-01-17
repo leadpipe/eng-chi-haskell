9bc2efd6d1cf4c883bdb98d3e5907860fafcd168
Jeremy@marzhillstudios.com <Jeremy@marzhillstudios.com@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-01-17 05:25:48 +0000

    safe versions of the partial list functions

--- jwall/ch04/Exercises.hs
+++ jwall/ch04/Exercises.hs
@@ -1,1 +1,28 @@
 --file: ch04/Exercises.hs
+
+data Item a = Item a
+            | Undefined
+            deriving (Show)
+
+--safeHead
+
+safeHead [] = Undefined
+safeHead (x:xs) = Item x
+
+--safeTail
+
+safeTail [] = []
+safeTail (x:xs) = xs
+
+--safeLast
+
+safeLast [] = Undefined
+safeLast (x:[]) = Item x
+safeLast (x:xs) = safeLast(xs)
+
+--safeInit
+
+safeInit [] = [Undefined]
+safeInit (x1:x2:[]) = [Item x1]
+safeInit (x:xs) = Item x:safeInit(xs)
+
