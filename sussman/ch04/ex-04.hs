35a24e791fcae7e09e603e5dd7da6e8057213848
sussman <sussman@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-02-10 05:31:42 +0000

    ch04 ex04

---
+++ sussman/ch04/ex-04.hs
@@ -0,0 +1,33 @@
+
+-- Write a program that transposes the text in a file. For instance,
+-- it should convert "hello\nworld\n" to "hw\neo\nlr\nll\nod\n".
+
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = transposeText
+
+
+peelHead :: [[Char]] -> ([Char], [[Char]])
+peelHead [] = ("", [])
+peelHead strlist = (map head strlist, map (drop 1) strlist)
+
+buildLines :: [String] -> [String]
+buildLines [] = []
+buildLines [""] = []
+buildlines somelines = peeledword : buildLines shorterlines
+  where (peeledword, shorterlines) = peelHead somelines
+
+transposeText :: String -> String
+transposeText str = unlines (buildLines (lines str))
