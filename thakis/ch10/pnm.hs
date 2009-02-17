10d44b0accfb8a6481684532ca29ea0576a91701
nicothakis <nicothakis@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-02-17 01:29:42 +0000

    add second parsepgm version, and a wip version of the third

--- thakis/ch10/pnm.hs
+++ thakis/ch10/pnm.hs
@@ -1,6 +1,8 @@
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
 import Data.Char (isSpace)
+import Data.Int (Int64)
+import Data.Word (Word8)
 
 data Greymap = Greymap {
       greyWidth :: Int
@@ -44,6 +46,7 @@ matchHeader prefix str
     | otherwise
         = Nothing
 
+
 ------------------------------------------------------------------------------
 -- First version: Explicit error checking ------------------------------------
 ------------------------------------------------------------------------------
@@ -77,9 +80,122 @@ parseP5_naive s =
                             Nothing -> Nothing
                             Just (bitmapData, s6) ->
                               Just (Greymap width height maxGrey bitmapData, s6)
-                
 
-parseP5 = parseP5_naive
+
+------------------------------------------------------------------------------
+-- Second version: extract Maybe handling ------------------------------------
+------------------------------------------------------------------------------
+
+-- The "Maybe" wrapper
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v >>? f = f v
+
+-- Helper
+skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
+skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
+
+
+-- below, the code
+-- a >>?
+-- b >>?
+-- c
+--
+-- is parsed as
+--
+-- ((a >>? b) >>? c).
+
+parseP5_take2 s =
+  matchHeader (L8.pack "P5") s >>?
+  \s -> skipSpace ((), s) >>?
+  (getNat . snd) >>?
+  skipSpace >>?
+  \(width, s) -> getNat s >>?
+  skipSpace >>?
+  \(height, s) -> getNat s >>?
+  \(maxGrey, s) -> getBytes 1 s >>?
+  (getBytes (width * height) . snd) >>?
+  \(bitmapData, s) -> Just (Greymap width height maxGrey bitmapData, s)
+
+
+------------------------------------------------------------------------------
+-- Third version: Hiding the parse state -------------------------------------
+------------------------------------------------------------------------------
+
+-- Instead of returning `Maybe (some, pair)` as parse state from our parsing
+-- functions, we return an algebraic data type instead. This makes it easier
+-- to add more fields to the state because the parsing functions don't have to
+-- do pattern matching to get access to the parse state (and hence, changing
+-- the parse state does no longer break all parsing functions).
+
+data ParseState = ParseState {
+      string :: L.ByteString
+    , offset :: Int64
+    } deriving (Show)
+
+-- A parse function takes a ParseState and produces a piece of data and a new
+-- ParseState. It can also fail, in which case it returns an error message
+-- (through Either).
+newtype Parse a = Parse {
+      runParse :: ParseState -> Either String (a, ParseState)
+    }
+
+-- Returns a parse function that returns the given value `val`
+identity :: a -> Parse a
+identity val = Parse (\s -> Right (val, s))
+
+-- A parser that reads a single byte
+-- (XXX: this uses a quite convoluted approach?)
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+    case L.uncons (string initState) of
+      Nothing ->
+          bail "no more input"
+      Just (byte,remainder) ->
+          putState newState ==> \_ ->
+          identity byte
+        where newState = initState { string = remainder,
+                                     offset = newOffset }
+              newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+              
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+  
+-- Chain two parsers: Given a parser that extracts an `a` and a function that
+-- maps `a` to a parser that returns `b`, this returns a parser that returns
+-- a `b`. Effectively, executes the first parser and feeds the result in the
+-- function.
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser  =  Parse chainedParser
+  where chainedParser initState   =  -- note that we omit the state param here!
+          case runParse firstParser initState of
+            Left errMessage ->
+                Left errMessage
+            Right (firstResult, newState) ->
+                runParse (secondParser firstResult) newState
+
+-- Applies a regular function to a value returned by a parser
+instance Functor Parse where
+    fmap f parser = parser ==> \result ->
+                    identity (f result)
+
+
+                
+------------------------------------------------------------------------------
+-- Main function -------------------------------------------------------------
+------------------------------------------------------------------------------
+
+--parseP5 = parseP5_naive
+parseP5 = parseP5_take2
+
 main = do
   -- I created the pbm file like this:
   -- $ sudo port install netpbm
@@ -87,6 +203,8 @@ main = do
   -- $ ppmtopgm test.ppm > test.pgm
   -- Alternatively, install imagemagick and do
   -- $ convert myfile.tiff test.pgm
+  -- (For the tiff file, I opened some image in Preview.app and dragged its
+  -- proxy icon in the title bar into Terminal.app)
   f <- L.readFile "test.pgm"
   let r = parseP5_naive f
   print r
