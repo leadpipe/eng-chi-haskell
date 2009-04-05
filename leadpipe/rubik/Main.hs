922ca44bdd5d469dd7aa71c5885a391ae49107e4
luke.blanshard <luke.blanshard@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-04-05 20:35:49 +0000

    Try looking for "closing" moves ahead of pure random ones

--- leadpipe/rubik/Main.hs
+++ leadpipe/rubik/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Rubik
@@ -7,28 +8,36 @@ import qualified Data.Set as Set
 import System.Random
 
 findSequence :: RandomGen g => g -> Set.Set [(Int, Int)] -> ([(Int, Int)], g)
-findSequence g s = fs g 1 [first] False (movePerms!first)
-    where first = (2, 3) -- WLOG, start with F3
+findSequence g s = fs g 1 [first] False (movePerms!first) counts
+    where first = (2, 3) -- WLOG, start with f
           maxlen = 20
-          fs g n seq@((lastFace,_):_) opp perm
+          counts = (array (0,5) [(i,0) | i <- [0..5]]) // [first]
+          fs g n seq@((lastFace,_):_) opp perm counts
               | n > maxlen
                   = ([], g)
               | movesOnlyBottom perm
                   = (reverse seq, g)
               | otherwise
-                  = let (next@(nextFace,_), g') = genMove g lastFace opp
+                  = let (next@(nextFace,_), g') = genMove g counts lastFace opp
                         opp' = (nextFace `isOpposite` lastFace)
                         perm' = movePerms!next
-                        a@(ms, g'') = fs g' (n+1) (next:seq) opp' (perm *> perm')
+                        counts' = accum sum4 counts [next]
+                        sum4 a b = (a+b) `mod` 4
+                        a@(ms, g'') = fs g' (n+1) (next:seq) opp' (perm *> perm') counts'
                     in if null ms || fst(random g) || not (Set.member ms s)
                        then a
-                       else fs g'' n seq opp perm -- backtrack and try again
-          genMove g face opp =
+                       else fs g'' n seq opp perm counts -- backtrack and try again
+          genMove g counts face opp =
               let (nf, g') = randomR (0,5) g
-              in if nf == face || opp && nf == oppositeFace face
-                 then genMove g' face opp
-                 else let (rot, g'') = randomR (1,3) g'
-                      in ((nf, rot), g'')
+                  (r, g'') = genRotation g' counts nf
+              in  if nf == face || opp && nf `isOpposite` face
+                  then genMove g'' counts face opp
+                  else ((nf, r), g'')
+          genRotation g counts nf =
+              let (p::Double, g') = random g
+                  c = counts ! nf
+              in  if p < 0.7 && c > 0 then (4-c, g')
+                  else randomR (1,3) g'
 
 main = do
   g <- getStdGen
@@ -48,5 +57,5 @@ main = do
 
         readFoundFile = do
           lines <- readFile "found.txt" >>= return.lines
-          let moveSeqs = map (toMoves'.head.words) lines
+          let moveSeqs = map (toMoves.head.words) lines
           return (Set.fromList moveSeqs)
