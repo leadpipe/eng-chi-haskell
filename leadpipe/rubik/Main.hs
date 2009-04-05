df5219bd6f111dcc3a70e06b20e651a4bd56ffd7
luke.blanshard <luke.blanshard@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2009-04-05 17:59:09 +0000

    Have the option of backtracking when we find a previously found sequence

--- leadpipe/rubik/Main.hs
+++ leadpipe/rubik/Main.hs
@@ -6,17 +6,23 @@ import Data.Array
 import qualified Data.Set as Set
 import System.Random
 
-findSequence :: RandomGen g => g -> ([(Int, Int)], g)
-findSequence g = fs g 1 [first] False (movePerms!first)
+findSequence :: RandomGen g => g -> Set.Set [(Int, Int)] -> ([(Int, Int)], g)
+findSequence g s = fs g 1 [first] False (movePerms!first)
     where first = (2, 3) -- WLOG, start with F3
           maxlen = 20
           fs g n seq@((lastFace,_):_) opp perm
-              | n > maxlen = ([], g)
-              | movesOnlyBottom perm = (reverse seq, g)
-              | otherwise = let (next@(nextFace,_), g') = genMove g lastFace opp
-                                opp' = (nextFace `isOpposite` lastFace)
-                                perm' = movePerms!next
-                            in fs g' (n+1) (next:seq) opp' (perm *> perm')
+              | n > maxlen
+                  = ([], g)
+              | movesOnlyBottom perm
+                  = (reverse seq, g)
+              | otherwise
+                  = let (next@(nextFace,_), g') = genMove g lastFace opp
+                        opp' = (nextFace `isOpposite` lastFace)
+                        perm' = movePerms!next
+                        a@(ms, g'') = fs g' (n+1) (next:seq) opp' (perm *> perm')
+                    in if null ms || fst(random g) || not (Set.member ms s)
+                       then a
+                       else fs g'' n seq opp perm -- backtrack and try again
           genMove g face opp =
               let (nf, g') = randomR (0,5) g
               in if nf == face || opp && nf == oppositeFace face
@@ -26,10 +32,11 @@ findSequence g = fs g 1 [first] False (movePerms!first)
 
 main = do
   g <- getStdGen
-  findSequences 0 g Set.empty
+  found <- readFoundFile
+  findSequences 0 g found
       where
         findSequences n g s = do
-          let (seq, g') = findSequence g
+          let (seq, g') = findSequence g s
               s' = Set.insert seq s
           if n `mod` 1000000 == 0
             then putStrLn (show n)
@@ -38,3 +45,8 @@ main = do
             then return ()
             else evm seq
           findSequences (n+1) g' s'
+
+        readFoundFile = do
+          lines <- readFile "found.txt" >>= return.lines
+          let moveSeqs = map (toMoves'.head.words) lines
+          return (Set.fromList moveSeqs)
