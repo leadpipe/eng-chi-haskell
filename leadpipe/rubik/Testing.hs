3e56cd12fceee4e97ea4634656bfe2f4386d53a1
luke.blanshard <luke.blanshard@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2011-01-01 22:18:14 +0000

    Fix this module to compile in the new ghc version

--- leadpipe/rubik/Testing.hs
+++ leadpipe/rubik/Testing.hs
@@ -1,8 +1,8 @@
-{-# OPTIONS_GHC -fglasgow-exts -XUndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances, ScopedTypeVariables, UndecidableInstances #-}
 module Testing where
 
 import Test.QuickCheck
 
-instance (Enum a, Bounded a) => Arbitrary a where
+instance forall a. (Enum a, Bounded a) => Arbitrary a where
   arbitrary = do n <- choose (fromEnum (minBound :: a), fromEnum (maxBound :: a) )
                  return $ toEnum n
