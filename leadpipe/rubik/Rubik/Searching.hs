4d6d6370c353f4bf5fdf5b887671856d0b54fe1a
luke.blanshard <luke.blanshard@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2011-01-19 04:02:54 +0000

    Clean up the search routine, and make the move generator a callback

--- leadpipe/rubik/Rubik/Searching.hs
+++ leadpipe/rubik/Rubik/Searching.hs
@@ -15,20 +15,21 @@ import qualified Data.Set as Set
 -- | Given an initial list of moves, searches a random tree of algorithms
 -- looking for instances that satisfy a given predicate.  The resulting list
 -- lives in the RandT + State monad.
-findAlgorithms :: (Puzzle s, RandomGen g, Random (Move s), Ord (Move s)) =>
+findAlgorithms :: (Puzzle s, RandomGen g, Ord (Move s)) =>
                   [Move s] -> (Algorithm s -> Bool) -> (Algorithm s -> Int -> Bool)
-                  -> Rand g [Algorithm s]
-findAlgorithms [] _ _ = return []
-findAlgorithms (m:ms) satisfying continue = liftM2 (++) fa (findAlgorithms ms satisfying continue)
-  where fa = fa' (applyMove one m)
+                  -> (Algorithm s -> Rand g (Move s)) -> Rand g [Algorithm s]
+findAlgorithms [] _ _ _ = return []
+findAlgorithms (m:ms) satisfying continue genMove = fa `append` findAlgorithms ms satisfying continue genMove
+  where append = liftM2 (++)
+        fa = fa' (applyMove one m)
         fa' alg = if satisfying alg then return [alg] else walkFrom alg 0 Set.empty
         --walkFrom :: Algorithm s -> Int -> Set (Move s) -> Rand g [Algorithm s]
         walkFrom alg count tried = if continue alg count then try alg count tried else return []
         try alg count tried =
-          do m <- getRandom
+          do m <- genMove alg
              if m `Set.member` tried then try alg count tried
                else let alg' = alg `applyMove` m
                         tried' = m `Set.insert` tried
-                        ans = if isNontrivial alg' && lastMove alg' == m
-                              then fa' alg' else return []
-                    in liftM2 (++) ans (walkFrom alg (count+1) tried')
+                    in if isNontrivial alg' && lastMove alg' == m
+                       then fa' alg' `append` walkFrom alg (count+1) tried'
+                       else walkFrom alg count tried'
