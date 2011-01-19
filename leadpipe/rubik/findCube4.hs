eea6e7ef3f6ca256344ee71937e8ab6ac3f098ef
luke.blanshard <luke.blanshard@4f4cb458-e31c-11dd-9ba5-89a75009fd5d>
2011-01-19 04:03:35 +0000

    Start work on generating more purposeful moves

--- leadpipe/rubik/findCube4.hs
+++ leadpipe/rubik/findCube4.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Rubik.Cube
@@ -6,24 +8,15 @@ import Rubik.Cycles
 import Rubik.Puzzle
 import Rubik.Searching
 
-import Control.Monad.Random (evalRandIO)
+import Control.Monad.Random
 import Data.List (sort)
 import System.Random
 
 main = do
-  let initialMoves = [read "f+", read "F+", read "f=", read "F="] :: [FaceTwist4]
-  algs <- evalRandIO $ findAlgorithms initialMoves movesTopEdges fanOut2to12
+  let initialMoves = map read ["f+", "F+", "f=", "F="] :: [FaceTwist4]
+  algs <- evalRandIO $ findAlgorithms initialMoves movesTopEdges (fanOut 2 20) genMove
   sequence_ [putStrLn (show alg) | alg <- algs]
 
-
-instance Random FaceTwist4 where
-  randomR ((FT4 f1 b1 t1), (FT4 f2 b2 t2)) g =
-    let (f, g1) = randomR (fromEnum f1, fromEnum f2) g
-        (b, g2) = randomR (fromEnum b1, fromEnum b2) g1
-        (t, g3) = randomR (toInteger t1, toInteger t2) g2
-    in (FT4 (toEnum f) (toEnum b) (fromInteger t), g3)
-
-  random = randomR ((FT4 minBound minBound 1), (FT4 maxBound maxBound maxBound))
 
 -- | Tells whether the given algorithm moves edge pieces only on the top (U)
 -- face.  It ignores what the algorithm does to vertices and face pieces.
@@ -31,10 +24,30 @@ movesTopEdges :: Algorithm Cube4 -> Bool
 movesTopEdges a = leavesUnmoved e nonTopEdgePieceIndices
   where Cube4 (_, e, _) = result a
 
-
 nonTopEdgePieceIndices :: [Int]
 nonTopEdgePieceIndices = sort [fromEnum ep | ep <- [minBound..], U `notElem` edgePieceFaces ep]
 
 
-fanOut2to12 :: Algorithm Cube4 -> Int -> Bool
-fanOut2to12 a n = n <= 2 && length (moves a) <= 12
+fanOut :: Int -> Int -> Algorithm Cube4 -> Int -> Bool
+fanOut width depth a n = n < width && length (moves a) <= depth
+
+
+-- | Generates a random move, sometimes using the given algorithm to close out
+-- the cumulative twist for a face.
+genMove :: Algorithm Cube4 -> Rand StdGen FaceTwist4
+genMove alg = do
+  (i :: Int) <- getRandomR (1, 10)
+  closeOuts <- calcCloseOuts alg
+  let numCloseOuts = length closeOuts
+  j <- getRandomR (0, numCloseOuts)
+  if i <= 7 && numCloseOuts > 0 then return (closeOuts !! j) else randomMove
+    where randomMove = do
+            f <- getRandomR (fromEnum (minBound::Face), fromEnum (maxBound::Face))
+            b <- getRandom
+            t <- getRandomR (1,3)
+            return (FT4 (toEnum f) b (fromInteger t))
+
+
+calcCloseOuts :: Algorithm Cube4 -> Rand StdGen [FaceTwist4]
+calcCloseOuts alg = do
+  return []
